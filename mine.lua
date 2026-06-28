d55e6b9f668530a578b26997366adfc57b2263ef
sethpeel240712-gif <sethpeel240712@gmail.com>
2026-06-29 00:06:40 +0100

    Update mine.lua

--- mine.lua
+++ mine.lua
@@ -32,11 +32,11 @@ EnableRCS = true --- When Set to false, it'll turn off Recoil Control!
 
 RecoilControlMode = "High" --- Presets: "Low", "Medium", "High", "Ultra", "Insanity", "Custom"
 
-RcCustomStrength = 7 -- Value MUST be ROUNDED! No Decimal values such as 6.5!
+RcCustomStrength = 20 -- Value MUST be ROUNDED! No Decimal values such as 6.5!
 
 RequireToggle = true -- Change to false if you want it to always be on.
 
-ToggleKey = "CapsLock" -- Usable Keys: "CapsLock", "NumLock", "ScrollLock"
+ToggleKey = "NumLock" -- Usable Keys: "CapsLock", "NumLock", "ScrollLock"
 
 DelayRate = 7 -- DO NOT CHANGE IF YOU DON'T KNOW WHAT YOU'RE DOING.
 
